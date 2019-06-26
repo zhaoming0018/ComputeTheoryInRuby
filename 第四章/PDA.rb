fd6d33f1b7a1bac65083d4469f6204c8d99424f7
zhaoming0018 <1786796646@qq.com>
2019-06-26 10:58:26 +0800

    增加了第四章DPDA和NPDA的代码

--- 第四章/PDA.rb
+++ 第四章/PDA.rb
@@ -25,6 +25,15 @@ end
 #   state : 当前所处的状态
 #   stack : 当前栈的状态
 class PDAConfiguration < Struct.new(:state, :stack)
+    STUCK_STATE = Object.new
+
+    def stuck
+        PDAConfiguration.new(STUCK_STATE, stack)
+    end
+
+    def stuck?
+        state == STUCK_STATE
+    end
 end
 
 # 记录一条PDA转移的规则
@@ -62,5 +71,61 @@ class DPDARulebook < Struct.new(:rules)
 
     def rule_for(configuration, character)
         rules.detect { |rule| rule.applies_to?(configuration, character)}
+    end
+
+    def applies_to?(configuration, character)
+        !rule_for(configuration, character).nil?
+    end
+
+    def follow_free_moves(configuration)
+        if applies_to?(configuration, nil)
+            follow_free_moves(next_configuration(configuration, nil))
+        else
+            configuration
+        end
+    end
+end
+
+class DPDA < Struct.new(:current_configuration, :accept_states, :rulebook)
+    def accepting?
+        accept_states.include?(current_configuration.state)
+    end
+
+    def read_character(character)
+        self.current_configuration = next_configuration(character)
+    end
+
+    def read_string(string)
+        string.chars.each do |character|
+            read_character(character) unless  stuck?
+        end
+    end
+
+    def current_configuration
+        rulebook.follow_free_moves(super)
+    end
+
+    def next_configuration(character)
+        if rulebook.applies_to?(current_configuration, character)
+            rulebook.next_configuration(current_configuration, character)
+        else
+            current_configuration.stuck
+        end
+    end
+
+    def stuck?
+        current_configuration.stuck?
+    end
+end
+
+class DPDADesign < Struct.new(:start_state, :bottom_character, :accept_states, :rulebook)
+    def accepts?(string)
+        to_dpda.tap { |dpda| dpda.read_string(string) }.accepting?
+    end
+
+    def to_dpda
+        start_stack = Stack.new([bottom_character])
+        start_configuration = PDAConfiguration.new(start_state, start_stack)
+        DPDA.new(start_configuration, accept_states, rulebook)
     end
 end
--- 第四章/main.rb
+++ 第四章/main.rb
@@ -1,7 +1,8 @@
 require './PDA.rb'
+require './NPDA.rb'
 
-rule = PDARule.new(1, '(', 2, '$', ['b', '$'])
-configuration = PDAConfiguration.new(1, Stack.new(['$']))
+# rule = PDARule.new(1, '(', 2, '$', ['b', '$'])
+# configuration = PDAConfiguration.new(1, Stack.new(['$']))
 
 # puts rule.push_characters
 # rule.follow(configuration)
@@ -13,9 +14,52 @@ rulebook = DPDARulebook.new([
     PDARule.new(2, nil, 1, '$', ['$'])
 ])
 
-configuration = rulebook.next_configuration(configuration, '(')
+# configuration = rulebook.next_configuration(configuration, '(')
+# puts configuration
+# configuration = rulebook.next_configuration(configuration, '(')
+# puts configuration
+# configuration = rulebook.next_configuration(configuration, ')')
+# puts configuration
+
+# dpda = DPDA.new(PDAConfiguration.new(1, Stack.new(['$'])), [1], rulebook)
+
+# dpda.read_string('(()')
+# puts dpda.accepting?
+
+# configuration = PDAConfiguration.new(2, Stack.new(['$']))
+# puts rulebook.follow_free_moves(configuration)
+
+# dpda_design = DPDADesign.new(1, '$', [1], rulebook)
+# puts dpda_design.accepts?('(((((((((())))))))))')
+# puts dpda_design.accepts?('()))(((((')
+# puts dpda_design.accepts?('()(())((()))()')
+
+rulebook = NPDARulebook.new([
+    PDARule.new(1, 'a', 1, '$', ['a', '$']),
+    PDARule.new(1, 'a', 1, 'a', ['a', 'a']),
+    PDARule.new(1, 'a', 1, 'b', ['a', 'b']),
+    PDARule.new(1, 'b', 1, '$', ['b', '$']),
+    PDARule.new(1, 'b', 1, 'a', ['b', 'a']),
+    PDARule.new(1, 'b', 1, 'b', ['b', 'b']),
+    PDARule.new(1, nil, 2, '$', ['$']),
+    PDARule.new(1, nil, 2, 'a', ['a']),
+    PDARule.new(1, nil, 2, 'b', ['b']),
+    PDARule.new(2, 'a', 2, 'a', []),
+    PDARule.new(2, 'b', 2, 'b', []),
+    PDARule.new(2, nil, 3, '$', ['$'])
+])
+
+configuration = PDAConfiguration.new(1, Stack.new(['$']))
 puts configuration
-configuration = rulebook.next_configuration(configuration, '(')
-puts configuration
-configuration = rulebook.next_configuration(configuration, ')')
-puts configuration
+npda = NPDA.new(Set[configuration], [3], rulebook)
+puts npda.accepting?
+puts npda.current_configurations
+npda.read_string('abb')
+puts npda.accepting?
+npda.read_string('a')
+puts npda.accepting?
+
+npda_design = NPDADesign.new(1, '$', [3], rulebook)
+puts npda_design.accepts?('abba')
+puts npda_design.accepts?('babbbbab')
+puts npda_design.accepts?('abb')
